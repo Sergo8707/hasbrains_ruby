62f4ef29b402d172358b84bb932f85024d91a7c5
Sergo8707 <sergo8707@gmail.com>
2017-01-04 11:57:55 +0200

    module

---
+++ item_container.rb
@@ -0,0 +1,32 @@
+module ItemContainer
+
+  module Manager # содержить все методы модифицирующие наш обэкт
+
+    def add_item(item)
+      @items.push item
+    end
+
+    def remove_item
+      @items.pop
+    end
+
+    def validate
+      @items.each { |i| puts "Item has no price" if i.price.nil? }
+    end
+
+    def delete_invalid_items
+      @items.delete_if { |i| i.price.nil? }
+    end
+
+  end
+
+  module Info #методы выводящие информацюю о товарах в нашем обэкте
+
+    def count_valid_items
+      @items.count { |i| i.price }
+    end
+
+  end
+
+
+end
