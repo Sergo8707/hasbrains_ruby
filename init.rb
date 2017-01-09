c9d3f3ac618b2009cf451707d56c88a5e70c5792
Sergo8707 <sergo8707@gmail.com>
2017-01-09 23:32:26 +0200

    Singletons and class methods

--- init.rb
+++ init.rb
@@ -1,12 +1,8 @@
-require_relative "string"
-require_relative "item_container"
-require_relative "item"
-require_relative "virtual_item"
-require_relative "real_item"
-require_relative "antique_item"
-require_relative "cart"
-require_relative "order"
-# 35
+require_relative "store_application"
+# 36
+
+puts StoreApplication.new == StoreApplication.new
+p StoreApplication.new
 @items =[]
 
 @items << AntiqueItem.new("car", price: 101, weight: 100)
